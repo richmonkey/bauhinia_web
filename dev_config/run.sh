c6e714a44a334c362b7071e1b2dfa74a2e6e1c38
houxh <houxuehua49@gmail.com>
2015-03-28 12:29:05 +0800

    更新启动脚本

--- dev_config/run.sh
+++ dev_config/run.sh
@@ -5,9 +5,9 @@ uwsgi=/usr/local/python2.7/bin/uwsgi
 home=/usr/local/python2.7
 app_dir=/vagrant/dev/im_api
 
-$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5000 -M  -p 1 -w im --callable app -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/im_api.log --pidfile /tmp/im_api.pid --touch-reload /tmp/im_api.touch
+$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :6000 -M  -p 1 -w im --callable app -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/bauhinia_api.log --pidfile /tmp/bauhinia_api.pid --touch-reload /tmp/bauhinia_api.touch
 
-$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5001 --gevent 1000 -M -p 1 -w webapp  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/im_web.log --pidfile /tmp/im_web.pid --touch-reload /tmp/im_web.touch
+$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :6001 --gevent 1000 -M -p 1 -w webapp  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/bauhinia_web.log --pidfile /tmp/bauhinia_web.pid --touch-reload /tmp/bauhinia_web.touch
 
-$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :5002 --gevent 1000 -M  -p 1 -w qr_login  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/qr_login.log --pidfile /tmp/qr_login.pid --touch-reload /tmp/qr_login.touch
+$uwsgi --uid nobody --gid nobody --chdir $app_dir --http :6002 --gevent 1000 -M  -p 1 -w qr_login  -t 60 --max-requests 5000 --vacuum --home $home --daemonize /tmp/qr_login.log --pidfile /tmp/qr_login.pid --touch-reload /tmp/qr_login.touch
 
--- dev_config/stop.sh
+++ dev_config/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 uwsgi=/usr/local/python2.7/bin/uwsgi
 
-$uwsgi --stop /tmp/im_api.pid
-$uwsgi --stop /tmp/im_web.pid
+$uwsgi --stop /tmp/bauhinia_api.pid
+$uwsgi --stop /tmp/bauhinia_web.pid
 $uwsgi --stop /tmp/qr_login.pid
